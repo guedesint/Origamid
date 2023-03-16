fcb12f0a51efcf1faf522f97afac1db9e5be80ec
GuedesNotbook <guedesint@gmail.com>
2023-03-16 20:02:12 -0300

    numeros e operadores

--- JavaScript/tipos-de-dados/script.js
+++ JavaScript/tipos-de-dados/script.js
@@ -1,2 +1,22 @@
+// Declare uma variável contendo uma string
 var nome = "Gabriel";
-var idade = 26;
+
+// Declare uma variável contendo um número dentro de uma string
+var anoDeNascimento = "1997";
+console.log(anoDeNascimento);
+// Declare uma variável com a sua idade
+var idade = 25;
+console.log(idade);
+// Declare duas variáveis, uma com seu nome
+var nome = 'Gabriel'
+console.log(nome);
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = 'Guedes'
+var nomeCompleto =`${nome} ${sobrenome}`
+console.log(nomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase ='It\'s time' 
+console.log(frase);
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
