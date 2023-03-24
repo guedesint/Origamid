1c5e87b7bb78e13a51ba9d8dbc3cd2585338d6dc
GuedesPC <guedesint@gmail.com>
2023-03-23 22:16:18 -0300

    codigo

---
+++ JavaScript/funcoes/script.js
@@ -0,0 +1,57 @@
+// Crie uma função para verificar se um valor é Truthy
+function isTruthy(dado) {
+  return !!dado;
+}
+
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+
+function quadrado(perimetro) {
+  var soma = perimetro * 4;
+  return console.log(`Seu perimetro tem ${soma} metros`);
+}
+
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+
+function identidade(primeiroNome, segundoNome) {
+  return console.log(`Seu nome completo é ${primeiroNome} ${segundoNome}`);
+}
+
+// Crie uma função que verifica se um número é par
+function numeroPar(valor) {
+  var valorTotal = valor % 2;
+
+  if (valorTotal == 0) {
+    console.log("par");
+  } else {
+    console.log("impar");
+  }
+}
+
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+function tipo(dado) {
+  return typeof dado;
+}
+
+
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'scroll' ocorrer.
+
+addEventListener('click', () => {
+  console.log('Gabriel Guedes');
+})
+
+
+// Corrija o erro abaixo
+function precisoVisitar(paisesVisitados) {
+  var totalPaises = 193;
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+
+  function jaVisitei(paisesVisitados) {
+    return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+  }
+}
