84fb58f5171a3105b8c69f655cb1c1a98a833a2a
Gabriel PC <guedesint@gmail.com>
2023-07-11 16:32:52 -0300

    1

---
+++ Objetos/native-host-e-user/script.js
@@ -0,0 +1,18 @@
+// Liste 5 objetos nativos
+
+Object
+String
+Array
+Function
+Date
+
+// Liste 5 objetos do browser
+Window
+history
+document
+HTMLCollection
+NodeList
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+webkitCancelAnimationFrame
+webkitSpeechGrammar
