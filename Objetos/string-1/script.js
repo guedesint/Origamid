6cb58f1cc47362fe385aa5ab43f0a4eed4c62395
GuedesNotbook <guedesint@gmail.com>
2023-07-27 10:42:30 -0300

    terminando string

--- Objetos/string-1/script.js
+++ Objetos/string-1/script.js
@@ -1,6 +1,5 @@
 // Utilizando o foreach na array abaixo,
 // some os valores de Taxa e os valores de Recebimento
-
 const transacoes = [
   {
     descricao: 'Taxa do Pão',
@@ -24,20 +23,59 @@ const transacoes = [
   },
 ];
 
+let taxaTotal = 0;
+let recebimentoTotal = 0;
+
+transacoes.forEach((item) => {
+ const numeroLimpo = +item.valor.replace('R$', '')
+ 
+  if (item.descricao.slice(0,4) === 'Taxa') {
+    taxaTotal += numeroLimpo;
+  }else if (item.descricao.slice(0,4) === 'Rece') {
+    recebimentoTotal += numeroLimpo;
+  }
+  
+});
+console.log(taxaTotal);
+console.log(recebimentoTotal);
+
+
 // Retorne uma array com a lista abaixo
 const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
 
+const arrayTransportes = transportes.split(';');
+console.log(arrayTransportes);
+
+
+
 // Substitua todos os span's por a's
-const html = `<ul>
+let html = `<ul>
                 <li><span>Sobre</span></li>
                 <li><span>Produtos</span></li>
                 <li><span>Contato</span></li>
               </ul>`;
 
+html = html.split('span').join('a').toLocaleLowerCase();
+console.log(html);
+
 // Retorne o último caracter da frase
 const frase = 'Melhor do ano!';
+console.log(frase.slice(-1));
 
 // Retorne o total de taxas
 const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
 
 
+let taxasTotal = 0;
+
+transacoes2.forEach((item) => {
+  item = item.toLocaleLowerCase();
+  item = item.trim();
+  item = item.slice(0 , 4)
+
+  if (item === 'taxa') {
+    taxasTotal++
+  }
+  
+});
+console.log(taxasTotal);
